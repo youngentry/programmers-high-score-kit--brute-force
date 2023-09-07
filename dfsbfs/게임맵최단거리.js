d0e3c666fc70ff5291f64f9480ee649c04a18ccc
youngentry <gentry_@naver.com>
2023-09-07 22:24:12 +0900

    feat: dfs

--- dfsbfs/게임맵최단거리.js
+++ dfsbfs/게임맵최단거리.js
@@ -1,33 +1,58 @@
 function solution(maps) {
-  const m = maps.length;
-  const n = maps[0].length;
+  // 0,0 에서 시작한다
 
-  const direction = [[-1, 0], [1, 0], [0, -1][(0, 1)]];
+  // 방문한 지점을 0으로 바꾼다
 
-  const p = [[0, 0, 0]];
+  // 방문하지 않은 지점으로 진행한다
+
+  // 이동횟수를 +1 한다
+
+  // 방문하지 않은 지점이 없다면 p의 위치가 n,m 에 위치했는지 확인한다
+
+  // n,m이 아니라면 -1을 반환한다
+
+  const [n, m] = [maps.length, maps[0].length];
+  if (maps[n - 1][m - 2] === 0 && maps[n - 2][m - 1] === 0) {
+    return -1;
+  }
+
+  const directions = [
+    [-1, 0], // 상
+    [1, 0], // 하
+    [0, -1], // 좌
+    [0, 1], // 우
+  ];
 
   maps[0][0] = 0;
 
-  while (p.length) {
-    console.log(maps);
-    const [x, y, move] = p.shift();
-    console.log(x, y, move);
+  let result = 10000;
 
-    if (x === n - 1 && y === m - 1) return move;
+  const dfs = (px, py, count, maps) => {
+    // maps[px][py] = 0; // 방문한 지점을 0으로 바꾼다
+    if (px === n - 1 && py === m - 1) {
+      result = Math.min(count, result);
+      return;
+    }
 
-    direction.forEach((dir) => {
-      const nextX = x + dir[0];
-      const nextY = y + dir[1];
-      if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n)
+    for (let dir of directions) {
+      const [dx, dy] = dir;
+      const [nextX, nextY] = [px + dx, py + dy];
+      if (nextX >= 0 && nextY >= 0 && nextX < n && nextY < m) {
         if (maps[nextX][nextY] === 1) {
-          maps[nextX][nextY] = 0;
-          p.push([nextX, nextY, move + 1]);
+          maps[nextX][nextY] = 0; // 방문한 지점을 0으로 바꾼다
+          dfs(nextX, nextY, count + 1, maps);
+          maps[nextX][nextY] = 1; // 방문한 지점을 0으로 바꾼다
         }
-    });
-  }
+      }
+    }
+  };
 
+  dfs(0, 0, 1, maps);
+
+  if (result !== 10000) return result;
   return -1;
 }
+
 console.log(
   solution([
     [1, 0, 1, 1, 1],
--- dfsbfs/타겟넘버.js
+++ dfsbfs/타겟넘버.js
@@ -1,44 +1,23 @@
 function solution(numbers, target) {
-  const operatorSet = new Set(); // 더하기 빼기로 조합할 수 있는 경우의 수 생성
-
   const numbersLength = numbers.length;
 
-  const dfs = (arr, pick) => {
-    if (pick === 0) {
-      operatorSet.add(arr.join(""));
+  let count = 0;
+
+  const dfs = (number, i) => {
+    if (i === numbersLength) {
+      if (number === target) {
+        count++;
+      }
       return;
     }
 
-    for (let j = 0; j < numbersLength; j++) {
-      arr[j] = "-"; // "-"로 바꾸어 주기
-      // arr[j] = "+"; // "+"로 되돌려 주기
-      dfs(arr, pick - 1);
-      arr[j] = "+"; // "+"로 바꾸어 주기
-    }
+    dfs(number + numbers[i], i + 1);
+    dfs(number - numbers[i], i + 1);
   };
 
-  for (let pick = 0; pick <= numbersLength; pick++) {
-    // "-"의 갯수 pick은 0개부터 5개까지 조합 가능
-    dfs([..."+".repeat(numbersLength)], pick);
-  }
-  console.log(operatorSet);
+  dfs(0, 0);
 
-  let result = 0;
-
-  operatorSet.forEach((operators) => {
-    let sum = 0;
-
-    const operatorArray = operators.split("");
-
-    operatorArray.forEach((each, index) => {
-      sum += eval(each + numbers[index]);
-    });
-
-    if (sum === target) result++;
-  });
-
-  console.log(result);
-  return result;
+  return count;
 }
 
 solution([1, 1, 1, 1, 1], 3);
