1d04934e606ed5bff372ee94f2e40b59bbfc8824
youngentry <gentry_@naver.com>
2023-09-12 02:11:08 +0900

    feat: dfs

--- dfsbfs/네트워크.js
+++ dfsbfs/네트워크.js
@@ -1,29 +1,42 @@
 const solution = (n, computers) => {
-  const visited = Array(computers.length).fill(0);
+  const obj = {}; // 각각의 컴퓨터와 연결된 네트워크를 기록
+  const visited = Array.from({ length: n }).fill(0);
 
-  let answer = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      // 자기 자신은 항상 1이므로 제외
+      if (i === j) continue;
 
-  computers.forEach((_, i) => {
-    if (!visited[i]) {
-      const stack = [i];
-
-      while (stack.length) {
-        const index = stack.pop();
-        if (!visited[index]) {
-          visited[index] = 1;
-
-          computers[index].forEach((computer, j) => {
-            console.log(computer, visited[j]);
-            if (computer && !visited[j]) {
-              stack.push(computer);
-            }
-          });
-        }
+      // i번째 컴퓨터와 연결된 컴퓨터의 index기록
+      if (computers[i][j]) {
+        obj[i] = obj[i] ? [...obj[i], j] : [j];
       }
-      answer++;
     }
-  });
-  console.log(answer);
+  }
+
+  console.log(obj);
+
+  let count = n; // 처음엔 모든 네트워크가 끊겨있다고 가정하고,
+
+  // 각각의 컴퓨터를 확인
+  for (let i in obj) {
+    const queue = [...obj[i]];
+    visited[i] = 1; // i번째 네트워크 방문처리
+
+    // 연결된 네트워크가 있을 때
+    while (queue.length) {
+      const next = queue.shift();
+
+      // 방문하지 않은 네트워크라면
+      if (!visited[next]) {
+        visited[next] = 1; // 다음번 네트워크 방문처리
+        queue.push(...obj[next]); // 해당 컴퓨터와 연결된 네트워크들을 추가
+        count--; // 네트워크 연결
+      }
+    }
+  }
+
+  return count;
 };
 
 solution(3, [
