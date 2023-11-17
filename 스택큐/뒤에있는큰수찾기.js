cd667fd1f5518774f326832552310f5b176a81e4
youngentry <gentry_@naver.com>
2023-11-17 18:08:12 +0900

    add 뒷큰수찾기lv2

--- 스택큐/뒤에있는큰수찾기.js
+++ 스택큐/뒤에있는큰수찾기.js
@@ -1,21 +1,20 @@
 function solution(numbers) {
-  const result = Array.from({ length: numbers.length }).fill(-1);
+  const result = Array(numbers.length).fill(-1);
   const stack = [];
 
   for (let i = 0; i < numbers.length; i++) {
-    console.log(stack, result);
-    console.log(stack[stack.length - 1], numbers[i]);
-    // 비교 숫자보다 작다면
-    while (stack[stack.length - 1] < numbers[i]) {
-      console.log(result, "?????");
-      result[stack.pop()] = numbers[i];
+    // 배열의 각 원소들에 대해 자신보다 뒤에 있는 숫자 중에서 자신보다 크면서 가장 가까운 수일 때
+    // 자신:    numbers[stack.at(-1)] (result에 할당되지 않은 index의 수는 numbers[index])
+    // 뒷 큰수: numbers[i]
+    while (stack.length && numbers[stack.at(-1)] < numbers[i]) {
+      const topIndex = stack.pop();
+      result[topIndex] = numbers[i];
     }
 
-    // result 요소를 변환할 인덱스 삽입
+    // result에 할당되지 않은 index를 stack에 추가
     stack.push(i);
   }
-
-  console.log(result);
+  return result;
 }
 
-solution([2, 3, 3, 5]);
+solution([9, 1, 5, 3, 6, 2]);
--- 스택큐/주식가격.js
+++ 스택큐/주식가격.js
@@ -1,32 +1,21 @@
 function solution(prices) {
-  const result = Array.from({ length: prices.length }).fill(0);
+  const size = prices.length;
 
+  const result = Array(prices.length).fill(0);
   const stack = [];
 
-  for (let i = 0; i < prices.length; i++) {
-    while (stack.length && prices[i] < stack[stack.length - 1]) {
-      const pop = stack.pop();
-      result[pop] = i - pop;
+  for (let i = 0; i < size; i++) {
+    while (stack.length && prices[stack.at(-1)] > prices[i]) {
+      const top = stack.pop();
+      result[top] = i - top;
     }
-
     stack.push(i);
   }
 
-  console.log(stack);
   while (stack.length) {
-    const pop = stack.pop();
-    result[pop] = prices.length - pop - 1;
+    const top = stack.pop();
+    result[top] = size - top - 1;
   }
-
-  console.log(result);
-  return result;
 }
 
-solution([1, 2, 3, 4, 3, 2, 1]);
-
-// 1,2,3,2에서 3보다 2가 작으니까 3을 제외하고 stack에서 마지막 제외
-// 1,2,3,1 이라면 stack에서 3,2 제외
-
-// 1. => 더이상 작지 않을 때까지 stack에서 제외한다.
-// 2. 시간 결정은 제외된 카운트를 하고,
-// 현재 index - 제외된 카운트의 수를 정한다.
+solution([1, 2, 3, 2, 3]);
