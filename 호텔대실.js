2e17e122651d308c7716f53e1cbe7b831591e6c2
youngentry <gentry_@naver.com>
2023-12-10 01:18:23 +0900

    feat: lv2호텔대실(20)

---
+++ 호텔대실.js
@@ -0,0 +1,34 @@
+function solution(book_time) {
+  const timeTable = Array(60 * 60).fill(0);
+
+  // [체크인시간, 퇴실시간+10] 구하기
+  const stayTimes = book_time.map((time) => {
+    const checkInMinute = getMinute(time[0]);
+    const checkOutMinute = getMinute(time[1]) + 10;
+    return [checkInMinute, checkOutMinute];
+  });
+
+  // 시간표 상에서 체크인시간index부터 퇴실시간index에 해당하는 요소에 +1 해주기
+  for (let i = 0; i < stayTimes.length; i++) {
+    for (let j = stayTimes[i][0]; j < stayTimes[i][1]; j++) {
+      timeTable[j]++;
+    }
+  }
+  return Math.max(...timeTable);
+
+  function getMinute(time) {
+    const [hour, min] = time.split(":");
+    return hour * 60 + Number(min);
+  }
+}
+
+// 최소한의 객실만 사용
+// 한 번 사용한 객실은 퇴실 시간을 기준으로 10분 청소 필요
+
+// 퇴실 시간에 10분을 더함
+// 겹치는 방의 수를 구함
+
+// [시작시간, 퇴실시간 + 10]을 구한다.
+// 3600개의 0을 지닌 배열을 만든다.
+// 이중 for문으로 (시작시간)~(퇴실시간+10) 구간의 요소에 ++ 해준다.
+// 최대 값을 Math.max()로 찾는다
